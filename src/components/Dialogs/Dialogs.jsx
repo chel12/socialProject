ccc96bb5766acf15136d938d6b90fc306aa9368a
chel12 <sema.chel12@gmail.com>
2023-09-05 11:22:52 +0300

    dialogs dev

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import classes from './Dialogs.module.css'
+import React from 'react';
+import classes from './Dialogs.module.css';
 const Dialogs = () => {
-  return (
-	 <div>Dialogs</div>
-  )
-}
+	return (
+		<div className={classes.dialogs}>
+			<div className={classes.dialogsItems}>
+				<div className={classes.dialog + '' + classes.active}>
+					1 man
+				</div>
+				<div className={classes.dialog}>2 man</div>
+				<div className={classes.dialog}>3 man</div>
+				<div className={classes.dialog}>3 man</div>
+				<div className={classes.dialog}>4 man</div>
+				<div className={classes.dialog}>5 man</div>
+				<div className={classes.dialog}>6 man</div>
+			</div>
+			<div className={classes.messages}>
+				<div className={classes.message}>Hello</div>
+				<div className={classes.message}>Whats you name?</div>
+				<div className={classes.message}>How are you ?</div>
+				<div className={classes.message}>haha</div>
+			</div>
+		</div>
+	);
+};
 
-export default Dialogs
+export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
-import classes from './Navbar.module.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import classes from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
-  return (
-	<nav className={classes.nav}>
-	<ul>
-		<li className={classes.item}>
-			<NavLink to={'/profile'}>Profile</NavLink>
-		</li>
-		<li className={classes.item}>
-		<NavLink to={'/dialogs'}>Dialogs</NavLink>
-		</li>
-		<li className={classes.item}>
-		<NavLink to={'/about'}>About</NavLink>
-		</li>
-		<li className={classes.item}>
-		<NavLink to={'/home'}>Home</NavLink>
-		</li>
-		<li className={classes.item}>
-		<NavLink to={'/contacts'}>Contacts</NavLink>
-		</li>
-	</ul>
-  </nav>
-  )
-}
+	return (
+		<nav className={classes.nav}>
+			<ul>
+				<li className={classes.item}>
+					<NavLink
+						to={'/profile'}
+						className={({ isActive }) =>
+							isActive ? classes.active : ''
+						}>
+						Profile
+					</NavLink>
+				</li>
+				<li className={classes.item}>
+					<NavLink
+						to={'/dialogs'}
+						className={({ isActive }) =>
+							isActive ? classes.active : ''
+						}>
+						Dialogs
+					</NavLink>
+				</li>
+				<li className={classes.item}>
+					<NavLink
+						to={'/about'}
+						className={({ isActive }) =>
+							isActive ? classes.active : ''
+						}>
+						About
+					</NavLink>
+				</li>
+				<li className={classes.item}>
+					<NavLink
+						to={'/home'}
+						className={({ isActive }) =>
+							isActive ? classes.active : ''
+						}>
+						Home
+					</NavLink>
+				</li>
+				<li className={classes.item}>
+					<NavLink
+						to={'/contacts'}
+						className={({ isActive }) =>
+							isActive ? classes.active : ''
+						}>
+						Contacts
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
