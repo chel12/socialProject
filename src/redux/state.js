8f6a6e3970d140885c2abed15c2174d3838f3b15
Семен Субочев <s.subochev@sz.ld>
2023-09-08 16:15:28 +0300

    refactor: index.js and render.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,36 +1,39 @@
+import { rerenderEntireTree } from "../render";
+
 let state = {
-	profilePage: {
-		postData: [
-			{ id: 1, message: 'Hello', likeCount: 2 },
-			{ id: 2, message: 'Moto Moto', likeCount: 12 },
-			{ id: 3, message: 'Ahaha,cool', likeCount: 22 },
-		],
-	},
-	dialogsPage: {
-		messagesData: [
-			{ id: 1, message: 'Hello' },
-			{ id: 2, message: 'You Best' },
-			{ id: 3, message: 'Ahaha,cool' },
-		],
-		dialogsData: [
-			{ id: 1, name: 'Dimas' },
-			{ id: 2, name: 'Pavel' },
-			{ id: 3, name: 'Piter' },
-			{ id: 4, name: 'Kolya' },
-			{ id: 5, name: 'Anna' },
-			{ id: 6, name: 'Olga' },
-			{ id: 7, name: 'Sveta' },
-			{ id: 8, name: 'Yura' },
-			{ id: 9, name: 'Pancho' },
-		],
-	},
+  profilePage: {
+    postData: [
+      { id: 1, message: "Hello", likeCount: 2 },
+      { id: 2, message: "Moto Moto", likeCount: 12 },
+      { id: 3, message: "Ahaha,cool", likeCount: 22 },
+    ],
+  },
+  dialogsPage: {
+    messagesData: [
+      { id: 1, message: "Hello" },
+      { id: 2, message: "You Best" },
+      { id: 3, message: "Ahaha,cool" },
+    ],
+    dialogsData: [
+      { id: 1, name: "Dimas" },
+      { id: 2, name: "Pavel" },
+      { id: 3, name: "Piter" },
+      { id: 4, name: "Kolya" },
+      { id: 5, name: "Anna" },
+      { id: 6, name: "Olga" },
+      { id: 7, name: "Sveta" },
+      { id: 8, name: "Yura" },
+      { id: 9, name: "Pancho" },
+    ],
+  },
 };
 export let addPost = (postMessage) => {
-	let newPost = {
-		id: 5,
-		message: postMessage,
-		likeCount: 0,
-	};
-	state.profilePage.postData.push(newPost);
+  let newPost = {
+    id: 5,
+    message: postMessage,
+    likeCount: 0,
+  };
+  state.profilePage.postData.push(newPost);
+  rerenderEntireTree(state);
 };
 export default state;
