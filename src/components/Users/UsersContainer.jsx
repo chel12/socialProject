0c19e8c7e7089282bc0c1f23dd3e30343ffaf4b5
chel12 <sema.chel12@gmail.com>
2023-09-18 20:26:53 +0300

    през и конт компоненты, теперь UsersContainer содержит класс котороый отправляет запросто во время монтирования компоненты и чистая презентационная Users

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Users from './Users';
 import { connect } from 'react-redux';
+import Users from './Users';
+import axios, * as others from 'axios';
 import {
 	followAC,
 	setCurrentPageAC,
@@ -8,6 +9,41 @@ import {
 	setUsersTotalCountAC,
 	unfollowAC,
 } from '../../redux/usersReducer';
+class UsersContainer extends React.Component {
+	componentDidMount() {
+		axios
+			.get(
+				`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+			)
+			.then((response) => {
+				this.props.setUsers(response.data.items);
+				this.props.setTotalUsersCount(response.data.totalCount);
+			});
+	}
+	onPageChanged = (pageNumber) => {
+		this.props.setCurrentPage(pageNumber);
+		axios
+			.get(
+				`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+			)
+			.then((response) => {
+				this.props.setUsers(response.data.items);
+			});
+	};
+	render() {
+		return (
+			<Users
+				totalUsersCount={this.props.totalUsersCount}
+				pageSize={this.props.pageSize}
+				currentPage={this.props.currentPage}
+				onPageChanged={this.onPageChanged}
+				users={this.props.users}
+				unfollow={this.props.unfollow}
+				follow={this.props.follow}
+			/>
+		);
+	}
+}
 
 let mapStateToProps = (state) => {
 	return {
@@ -38,4 +74,4 @@ let mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
