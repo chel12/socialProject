a28b9e5ce8b9887a8bff097ad4eee8f20e2d14ca
chel12 <sema.chel12@gmail.com>
2023-09-18 16:19:35 +0300

    запросы на сервак и примененеи их для пагинации, пагинация вручную изменена в цикле for на 10, чтобы не было 99999 страниц нахрен

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-import Users from "./Users";
-import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/usersReducer";
+import React from 'react';
+import Users from './Users';
+import { connect } from 'react-redux';
+import {
+	followAC,
+	setCurrentPageAC,
+	setUsersAC,
+	setUsersTotalCountAC,
+	unfollowAC,
+} from '../../redux/usersReducer';
 
 let mapStateToProps = (state) => {
-  return {
-    users: state.usersPage.users,
-  };
+	return {
+		users: state.usersPage.users,
+		pageSize: state.usersPage.pageSize,
+		totalUsersCount: state.usersPage.totalUsersCount,
+		currentPage: state.usersPage.currentPage,
+	};
 };
 
 let mapDispatchToProps = (dispatch) => {
-  return {
-    follow: (userId) => {
-      dispatch(followAC(userId));
-    },
-    unfollow: (userId) => {
-      dispatch(unfollowAC(userId));
-    },
-    setUsers: (users) => {
-      dispatch(setUsersAC(users));
-    },
-  };
+	return {
+		follow: (userId) => {
+			dispatch(followAC(userId));
+		},
+		unfollow: (userId) => {
+			dispatch(unfollowAC(userId));
+		},
+		setUsers: (users) => {
+			dispatch(setUsersAC(users));
+		},
+		setCurrentPage: (pageNumber) => {
+			dispatch(setCurrentPageAC(pageNumber));
+		},
+		setTotalUsersCount: (totalCount) => {
+			dispatch(setUsersTotalCountAC(totalCount));
+		},
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
