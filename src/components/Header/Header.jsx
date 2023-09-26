05629daa44974824c4bdd576806b2f9d989ca7a6
chel12 <sema.chel12@gmail.com>
2023-09-26 22:25:32 +0300

    cookie, login в теории, auth/me (если авторизован,то выводит логин)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import classes from "./Header.module.css";
-import { NavLink } from "react-router-dom";
-const Header = () => {
-  return (
-    <header className={classes.header}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"></img>
-      <div className={classes.loginBlock}>
-        <NavLink to={"/login"}>Login</NavLink>
-      </div>
-    </header>
-  );
+import React from 'react';
+import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+const Header = (props) => {
+	return (
+		<header className={classes.header}>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"></img>
+			<div className={classes.loginBlock}>
+				{props.isAuth ? (
+					props.login
+				) : (
+					<NavLink to={'/login'}>Login</NavLink>
+				)}
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,26 +1,28 @@
-const SET_USER_DATA = "SET_USER_DATA";
+const SET_USER_DATA = 'SET_USER_DATA';
 
 let initialState = {
-  userId: null,
-  email: null,
-  login: null,
+	userId: null,
+	email: null,
+	login: null,
+	isAuth: false,
 };
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_USER_DATA:
-      return {
-        ...state,
-        ...action.data,
-      };
+	switch (action.type) {
+		case SET_USER_DATA:
+			return {
+				...state,
+				...action.data,
+				isAuth: true,
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
-export const setUserData = (userId, email, login) => ({
-  type: SET_USER_DATA,
-  data: { userId, email, login },
+export const setAuthUserData = (userId, email, login) => ({
+	type: SET_USER_DATA,
+	data: { userId, email, login },
 });
 
 export default authReducer;
