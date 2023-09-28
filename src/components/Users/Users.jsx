a3f5c5b1b9c991d5179c5c0a935107ea08dcdb6f
chel12 <sema.chel12@gmail.com>
2023-09-28 23:19:46 +0300

    SANKI FInish Users components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,93 +1,84 @@
-import userPhoto from "./../../img/avatar-small.jfif";
-import React from "react";
-import styles from "./users.module.css";
-import { NavLink } from "react-router-dom";
-import axios, * as others from "axios";
-import { usersAPI } from "../../api/api";
+import userPhoto from './../../img/avatar-small.jfif';
+import React from 'react';
+import styles from './users.module.css';
+import { NavLink } from 'react-router-dom';
 let Users = (props) => {
-  //   let pagesCount = Math.ceil(props.setUsersTotalCount / props.pageSize);
-  let pages = [];
-  for (let i = 1; i <= 10; i++) {
-    pages.push(i);
-  }
-  return (
-    <div>
-      <div>
-        {pages.map((p) => {
-          return (
-            <span
-              onClick={(e) => {
-                props.onPageChanged(p);
-              }}
-              className={
-                props.currentPage === p
-                  ? styles.selectedPage
-                  : styles.paginationPage
-              }
-            >
-              {p}
-            </span>
-          );
-        })}
-      </div>
-      {props.users.map((u) => (
-        <div key={u.id} className={styles.usersItem}>
-          <span>
-            <div>
-              <NavLink to={"./../profile/" + u.id}>
-                <img
-                  src={u.photos.small != null ? u.photos.small : userPhoto}
-                  className={styles.userPhoto}
-                />
-              </NavLink>
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  disabled={props.followingInProgress.some((id) => id === u.id)}
-                  onClick={() => {
-                    props.toggleFollowingProgress(true, u.id);
-                    usersAPI.unfollow(u.id).then((response) => {
-                      if (response.data.resultCode === 0) {
-                        props.unfollow(u.id);
-                      }
-                      props.toggleFollowingProgress(false, u.id);
-                    });
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  disabled={props.followingInProgress.some((id) => id === u.id)}
-                  onClick={() => {
-                    props.toggleFollowingProgress(true, u.id);
-                    usersAPI.follow(u.id).then((response) => {
-                      if (response.data.resultCode === 0) {
-                        props.follow(u.id);
-                      }
-                      props.toggleFollowingProgress(false, u.id);
-                    });
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
+	//   let pagesCount = Math.ceil(props.setUsersTotalCount / props.pageSize);
+	let pages = [];
+	for (let i = 1; i <= 10; i++) {
+		pages.push(i);
+	}
+	return (
+		<div>
+			<div>
+				{pages.map((p) => {
+					return (
+						<span
+							onClick={(e) => {
+								props.onPageChanged(p);
+							}}
+							className={
+								props.currentPage === p
+									? styles.selectedPage
+									: styles.paginationPage
+							}>
+							{p}
+						</span>
+					);
+				})}
+			</div>
+			{props.users.map((u) => (
+				<div key={u.id} className={styles.usersItem}>
+					<span>
+						<div>
+							<NavLink to={'./../profile/' + u.id}>
+								<img
+									src={
+										u.photos.small != null
+											? u.photos.small
+											: userPhoto
+									}
+									className={styles.userPhoto}
+								/>
+							</NavLink>
+						</div>
+						<div>
+							{u.followed ? (
+								<button
+									disabled={props.followingInProgress.some(
+										(id) => id === u.id
+									)}
+									onClick={() => {
+										props.unfollow(u.id);
+									}}>
+									Unfollow
+								</button>
+							) : (
+								<button
+									disabled={props.followingInProgress.some(
+										(id) => id === u.id
+									)}
+									onClick={() => {
+										props.follow(u.id);
+									}}>
+									Follow
+								</button>
+							)}
+						</div>
+					</span>
+					<span>
+						<span>
+							<div>{u.name}</div>
+							<div>{u.status}</div>
+						</span>
+						<span>
+							<div>{'u.location.country'}</div>
+							<div>{'u.location.city'}</div>
+						</span>
+					</span>
+				</div>
+			))}
+		</div>
+	);
 };
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,59 +1,58 @@
-import React from "react";
-import { connect } from "react-redux";
-import Users from "./Users";
+import React from 'react';
+import { connect } from 'react-redux';
+import Users from './Users';
 import {
-  follow,
-  setCurrentPage,
-  unfollow,
-  getUsers,
-  toggleFollowingProgress,
-} from "../../redux/usersReducer";
-import Preloader from "../common/Preloader/Preloader";
+	follow,
+	setCurrentPage,
+	unfollow,
+	getUsers,
+	toggleFollowingProgress,
+} from '../../redux/usersReducer';
+import Preloader from '../common/Preloader/Preloader';
 class UsersContainer extends React.Component {
-  componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize);
-  }
-  onPageChanged = (pageNumber) => {
-    this.props.getUsers(this.props.pageNumber, this.props.pageSize);
-  };
-  render() {
-    return (
-      <>
-        {this.props.isFetching ? (
-          <Preloader />
-        ) : (
-          <Users
-            setUsersTotalCount={this.props.setUsersTotalCount}
-            pageSize={this.props.pageSize}
-            currentPage={this.props.currentPage}
-            onPageChanged={this.onPageChanged}
-            users={this.props.users}
-            unfollow={this.props.unfollow}
-            follow={this.props.follow}
-            toggleFollowingProgress={this.props.toggleFollowingProgress}
-            followingInProgress={this.props.followingInProgress}
-          />
-        )}
-      </>
-    );
-  }
+	componentDidMount() {
+		this.props.getUsers(this.props.currentPage, this.props.pageSize);
+	}
+	onPageChanged = (pageNumber) => {
+		this.props.getUsers(this.props.pageNumber, this.props.pageSize);
+	};
+	render() {
+		return (
+			<>
+				{this.props.isFetching ? (
+					<Preloader />
+				) : (
+					<Users
+						setUsersTotalCount={this.props.setUsersTotalCount}
+						pageSize={this.props.pageSize}
+						currentPage={this.props.currentPage}
+						onPageChanged={this.onPageChanged}
+						users={this.props.users}
+						unfollow={this.props.unfollow}
+						follow={this.props.follow}
+						followingInProgress={this.props.followingInProgress}
+					/>
+				)}
+			</>
+		);
+	}
 }
 
 let mapStateToProps = (state) => {
-  return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    setUsersTotalCount: state.usersPage.setUsersTotalCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress,
-  };
+	return {
+		users: state.usersPage.users,
+		pageSize: state.usersPage.pageSize,
+		setUsersTotalCount: state.usersPage.setUsersTotalCount,
+		currentPage: state.usersPage.currentPage,
+		isFetching: state.usersPage.isFetching,
+		followingInProgress: state.usersPage.followingInProgress,
+	};
 };
 
 export default connect(mapStateToProps, {
-  follow,
-  unfollow,
-  setCurrentPage,
-  toggleFollowingProgress,
-  getUsers,
+	follow,
+	unfollow,
+	setCurrentPage,
+	toggleFollowingProgress,
+	getUsers,
 })(UsersContainer);
