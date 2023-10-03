a9f2d17b32098fd2a395a51d2735e842b475a359
Семен Субочев <s.subochev@sz.ld>
2023-10-03 20:30:44 +0300

    )

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -4,18 +4,18 @@ class ProfileStatus extends React.Component {
   state = {
     editMode: false,
   };
-  activateEditMode() {
+  activateEditMode = () => {
     this.setState({ editMode: true });
-  }
-  deactivateEditMode() {
+  };
+  deactivateEditMode = () => {
     this.setState({ editMode: false });
-  }
+  };
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onDoubleClick={this.activateEditMode.bind(this)}>
+            <span onDoubleClick={this.activateEditMode}>
               {this.props.status}
             </span>
           </div>
@@ -24,7 +24,7 @@ class ProfileStatus extends React.Component {
           <div>
             <input
               autoFocus={true}
-              onBlur={this.deactivateEditMode.bind(this)}
+              onBlur={this.deactivateEditMode}
               value={this.props.status}
             ></input>
           </div>
