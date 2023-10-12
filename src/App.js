2b5b72f6bdf25fdc2a947894fc4eb9b91d00487d
chel12 <sema.chel12@gmail.com>
2023-10-12 18:59:54 +0300

    инициализация приложения

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import "./App.css";
-import React from "react";
-import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar/Navbar";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginPage from "./components/Login/Login";
-
-function App(props) {
-  return (
-    <div className="app-wrapper">
-      <HeaderContainer />
-      <Navbar />
-      <div className="app-wrapper-conten">
-        <Routes>
-          <Route path="/dialogs" element={<DialogsContainer />}></Route>
-          <Route path="/profile" element={<ProfileContainer />}>
-            <Route path=":userId" element={<ProfileContainer />} />
-          </Route>
-          <Route path="/users" element={<UsersContainer />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-        </Routes>
-      </div>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import Navbar from './components/Navbar/Navbar';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer, {
+	withRouter,
+} from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import LoginPage from './components/Login/Login';
+import { connect } from 'react-redux';
+import { initializeApp } from './redux/app-reducer';
+import { compose } from 'redux';
+import Preloader from './components/common/Preloader/Preloader';
+class App extends React.Component {
+	componentDidMount() {
+		this.props.initializeApp();
+	}
+	render() {
+		if (!this.props.initialized) {
+			return <Preloader />;
+		} else {
+			return (
+				<div className="app-wrapper">
+					<HeaderContainer />
+					<Navbar />
+					<div className="app-wrapper-conten">
+						<Routes>
+							<Route
+								path="/dialogs"
+								element={<DialogsContainer />}></Route>
+							<Route
+								path="/profile"
+								element={<ProfileContainer />}>
+								<Route
+									path=":userId"
+									element={<ProfileContainer />}
+								/>
+							</Route>
+							<Route
+								path="/users"
+								element={<UsersContainer />}></Route>
+							<Route
+								path="/login"
+								element={<LoginPage />}></Route>
+						</Routes>
+					</div>
+				</div>
+			);
+		}
+	}
 }
+const mapStateToProps = (state) => ({ initialized: state.app.initialized });
 
-export default App;
+export default compose(
+	withRouter,
+	connect(mapStateToProps, { initializeApp })
+)(App);
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,22 +1,24 @@
 import {
-  applyMiddleware,
-  combineReducers,
-  legacy_createStore as createStore,
-} from "redux";
+	applyMiddleware,
+	combineReducers,
+	legacy_createStore as createStore,
+} from 'redux';
 
-import profileReducer from "./profileReducer";
-import dialogsReducer from "./dialogsReducer";
-import usersReducer from "./usersReducer";
-import authReducer from "./authReducer";
-import thunkMiddleware from "redux-thunk";
-import { reducer as formReducer } from "redux-form";
+import profileReducer from './profileReducer';
+import dialogsReducer from './dialogsReducer';
+import usersReducer from './usersReducer';
+import authReducer from './authReducer';
+import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
+import appReducer from './app-reducer';
 
 let reducers = combineReducers({
-  profilePage: profileReducer,
-  dialogsPage: dialogsReducer,
-  usersPage: usersReducer,
-  auth: authReducer,
-  form: formReducer,
+	profilePage: profileReducer,
+	dialogsPage: dialogsReducer,
+	usersPage: usersReducer,
+	auth: authReducer,
+	form: formReducer,
+	app: appReducer,
 });
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 export default store;
