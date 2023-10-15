203eef3bfad1e8970758d15496fd1a4896512cc4
Семен Субочев <s.subochev@sz.ld>
2023-10-15 15:44:45 +0300

    подключаем reselect (reselect часть 3)

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,18 +1,28 @@
-export const getUsers= (state) => {
-	return state.usersPage.users;
+import { createSelector } from "reselect";
+
+const getUsersSelector = (state) => {
+  return state.usersPage.users;
 };
+export const getUsers = createSelector(getUsersSelector, (users) => {
+  return users.filter((u) => true);
+});
 export const getPageSize = (state) => {
-	return state.usersPage.pageSize;
+  return state.usersPage.pageSize;
 };
 export const getUsersTotalCount = (state) => {
-	return state.usersPage.setUsersTotalCount;
+  return state.usersPage.setUsersTotalCount;
 };
 export const getCurrentPage = (state) => {
-	return state.usersPage.currentPage;
+  return state.usersPage.currentPage;
 };
 export const getIsFetching = (state) => {
-	return state.usersPage.isFetching;
+  return state.usersPage.isFetching;
 };
 export const GetFollowingInProgress = (state) => {
-	return state.usersPage.followingInProgress;
+  return state.usersPage.followingInProgress;
 };
+
+/* biblioteka reselect */
+// export const getUsersSuper = createSelector(getUsers, (users) => {
+//   users.filter((u) => true);
+// });
